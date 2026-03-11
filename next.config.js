b445eccb8f10fd7629efe1a6d3c2718ff37f3937
Nagargoje Abhishek <nagargojeabhishek96@gmail.com>
2026-03-12 02:03:15 +0530

    Push a code on github

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const repo = "abhishek-nagargoje-portfolio";
+
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  output: "export",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+  images: {
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig;
