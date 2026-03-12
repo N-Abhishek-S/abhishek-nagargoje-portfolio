85d104ccef5504ef89ba6715b8397a162fe8a29b
Nagargoje Abhishek <nagargojeabhishek96@gmail.com>
2026-03-12 17:23:45 +0530

    git push

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-/** @type {import('next').NextConfig} */
-
 const repo = "abhishek-nagargoje-portfolio";
 const isProd = process.env.NODE_ENV === "production";
 
@@ -7,9 +5,7 @@ const nextConfig = {
   output: "export",
   basePath: isProd ? `/${repo}` : "",
   assetPrefix: isProd ? `/${repo}/` : "",
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
